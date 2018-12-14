488ef3811d56f1ca60dcda1fbb0eb1b491203d6f
Lam Hieu <lamhieu.vk@gmail.com>
2018-12-14 14:09:09 +0700

    fix: workflow to run callbacks for virtual subscriptions

--- hypersubs.js
+++ hypersubs.js
@@ -1,10 +1,26 @@
 import { Random } from 'meteor/random'
 import { EJSON } from 'meteor/ejson'
 import { Tracker } from 'meteor/tracker'
-import { slice, hasOwn, generateToken, isFunction } from './utils'
+import { argsParser, hasOwn, generateToken, checkCallbacksIsValid } from './utils'
+
+export const CONST = {
+  CALLBACK_TYPES: {
+    ON_READY: 'onReady',
+    ON_ERROR: 'onError',
+    ON_STOP: 'onStop',
+  },
+  CALLBACK_NAMES: {
+    onReady: 'readyCallback',
+    onError: 'errorCallback',
+    onStop: 'stopCallback',
+  },
+}
 
 export const name = 'hypersubs'
 
+/**
+ * Apply hypersubs worker
+ */
 function applyHyperSubs() {
   const shouldPreventBinding = !!(Meteor.subscribe && Meteor.subscribe.isHyperSubs)
   if (shouldPreventBinding) {
@@ -14,24 +30,11 @@ function applyHyperSubs() {
   const originalConnection = Meteor.connection
   const originalSubscribe = Meteor.subscribe
 
-  function argsParser(name) {
-    const params = slice.call(arguments, 1)
-    let callbacks = Object.create(null)
-    if (params.length) {
-      const lastParam = params[params.length - 1]
-      if (isFunction(lastParam)) {
-        callbacks.onReady = params.pop()
-      } else if (lastParam && [lastParam.onReady, lastParam.onError, lastParam.onStop].some(isFunction)) {
-        callbacks = params.pop()
-      }
-    }
-    return {
-      name,
-      params,
-      callbacks
-    }
-  }
-
+  /**
+   * Create a subscription
+   * @param {Object} data data
+   * @param {Object} options options
+   */
   function createSubscription(data, options) {
     const self = this
 
@@ -39,6 +42,16 @@ function applyHyperSubs() {
     const { isOriginalSubscription = false } = options
 
     const tokenOfSubscription = generateToken([name, ...params])
+
+    /**
+     * Get ready staus of real subscription
+     */
+    const getReadyOfRealSubscription = () => {
+      if (!self._listHandleOfHyperSubs) self._listHandleOfHyperSubs = {}
+      const handleOfHyperSubs = self._listHandleOfHyperSubs[tokenOfSubscription]
+      const handleIsReady = !!(handleOfHyperSubs && handleOfHyperSubs.ready())
+      return handleIsReady
+    }
 
     const checkIfSubscriptionExisted = sub => sub.inactive && sub.name === name && EJSON.equals(sub.params, params)
     let existing = Object.values(self._subscriptions).find(checkIfSubscriptionExisted)
@@ -90,10 +103,23 @@ function applyHyperSubs() {
           if (callbacks.onStop) {
             callbacks.onStop()
           }
-        }
+        },
+        isOriginalSubscription,
       }
+
       if (isOriginalSubscription) {
         self._send({ msg: 'sub', id: id, name: name, params: params })
+      } else {
+        // if create new virtual subscription, check and trigger based real subscription existed before
+        const handleIsReady = getReadyOfRealSubscription()
+        const record = self._subscriptions[id]
+        if (handleIsReady && !record.ready && !record.inactive) {
+          record.ready = true
+          if (callbacks.onReady) {
+            callbacks.onReady()
+          }
+          record.readyDeps.changed()
+        }
       }
     }
 
@@ -112,25 +138,30 @@ function applyHyperSubs() {
         record.readyDeps.depend()
         return record.ready
       },
-      subscriptionId: id
+      subscriptionId: id,
     }
 
+    const listSubscriptions = Object.values(self._subscriptions)
+
+    /**
+     * Check should stop original subscription
+     */
     const checkShouldStopOriginalSubscription = () =>
-      Object.values(self._subscriptions).filter(sub => sub.token === tokenOfSubscription).length <= 1
+      listSubscriptions.filter(sub => sub.token === tokenOfSubscription).length <= 1
 
     const shouldTrackingForVirtualSubscription = !isOriginalSubscription && Tracker.active
     if (shouldTrackingForVirtualSubscription) {
       const trackingHandleOfHyperSubs = Tracker.autorun(computation => {
-        const handleOfHyperSubs = self._listHandleOfHyperSubs[tokenOfSubscription]
-        const handleIsReady = handleOfHyperSubs.ready()
+        const handleIsReady = getReadyOfRealSubscription()
         const record = self._subscriptions[id]
         const isSameStateOfReady = record.ready === handleIsReady
         const isFirstRun = computation.firstRun
-        if (!record.inactive && (isFirstRun || !isSameStateOfReady)) {
+        if (isFirstRun || (!record.inactive && !isSameStateOfReady)) {
           record.ready = handleIsReady
           record.readyDeps.changed()
         }
       })
+
       Tracker.onInvalidate(() => {
         if (hasOwn.call(self._subscriptions, id)) {
           self._subscriptions[id].inactive = true
@@ -153,22 +184,60 @@ function applyHyperSubs() {
     return handle
   }
 
-  // create function override Meteor subscribe
+  /**
+   * Create function override Meteor subscribe
+   */
   function overrideSubscribe() {
     const self = this
 
     const data = argsParser.apply(self, arguments)
-    const { name, params } = data
+    const { name, params, callbacks } = data
 
     const tokenOfSubscription = generateToken([name, ...params])
     self._listHandleOfHyperSubs = self._listHandleOfHyperSubs || {}
     if (!hasOwn.call(self._listHandleOfHyperSubs, tokenOfSubscription)) {
       // create real subscription to get data
-      self._listHandleOfHyperSubs[tokenOfSubscription] = createSubscription.apply(self, [
-        { name, params },
-        {
-          isOriginalSubscription: true
+      let overrideCallbacks
+
+      const isHaveCallbacks = checkCallbacksIsValid(callbacks)
+      if (isHaveCallbacks) {
+        const typeOfCallbacks = Object.values(CONST.CALLBACK_TYPES)
+        const callbackReducer = (acc, typeOfCallback) => {
+          if (!callbacks[typeOfCallback]) return acc
+          const nameOfCallback = CONST.CALLBACK_NAMES[typeOfCallback]
+          function callback() {
+            const args = arguments
+            const listSubscriptions = Object.values(self._subscriptions)
+            const checkIfCallbackValid = sub => !sub.isOriginalSubscription && sub.token === tokenOfSubscription
+            const subscriptions = listSubscriptions.filter(checkIfCallbackValid)
+            subscriptions.forEach(sub => {
+              const callback = sub[nameOfCallback]
+              if (!callback) return false
+
+              if (typeOfCallback === CONST.CALLBACK_TYPES.ON_READY) {
+                if (sub.ready) return false
+                sub.ready = true
+                callback.apply(this, args)
+                sub.readyDeps.changed()
+                return true
+              }
+              callback.apply(this, args)
+              return true
+            })
+          }
+          return {
+            ...acc,
+            [typeOfCallback]: callback,
+          }
         }
+        overrideCallbacks = typeOfCallbacks.reduce(callbackReducer, Object.create(null))
+      }
+
+      self._listHandleOfHyperSubs[tokenOfSubscription] = createSubscription.apply(self, [
+        { name, params, callbacks: overrideCallbacks },
+        {
+          isOriginalSubscription: true,
+        },
       ])
     }
 
@@ -176,8 +245,8 @@ function applyHyperSubs() {
     const handle = createSubscription.apply(self, [
       data,
       {
-        isOriginalSubscription: false
-      }
+        isOriginalSubscription: false,
+      },
     ])
 
     return handle
--- utils.js
+++ utils.js
@@ -7,3 +7,25 @@ export const slice = Array.prototype.slice
 export const isFunction = f => typeof f === 'function'
 
 export const generateToken = params => hashObject([...params])
+
+export const checkCallbacksIsValid = callbacks =>
+  typeof callbacks === 'object' && Object.values(callbacks).filter(Boolean).length > 0
+
+export function argsParser(name) {
+  const params = slice.call(arguments, 1)
+  let callbacks = Object.create(null)
+  if (params.length) {
+    const lastParam = params[params.length - 1]
+    const listCallbacks = [lastParam.onReady, lastParam.onError, lastParam.onStop]
+    if (isFunction(lastParam)) {
+      callbacks.onReady = params.pop()
+    } else if (lastParam && listCallbacks.some(isFunction)) {
+      callbacks = params.pop()
+    }
+  }
+  return {
+    name,
+    params,
+    callbacks,
+  }
+}
